f47ff007f49a3ef479e3266bbcc3623a2123be5c
FloralSoda <lilysoda@pm.me>
2023-11-11 02:28:23 +0000

    FIxed packer.sh

--- packer.sh
+++ packer.sh
@@ -1,39 +1,59 @@
 #!/bin/bash
-$ret = 0
-$msg = "Enter number next to function: "
-input () {
+ret=0
+msg="Enter number next to function: "
+upper=2
+lower=1
+get_input () {
 	while :; do
-		read -p $msg ret
+		read -p "$msg" ret
 		[[ $ret =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
-		if ((ret >= 1 && ret <= 2)); then
+		if ((ret >= $lower && ret <= $upper)); then
 			break
 		else
 			echo "Input out of range, please try again"
 		fi
 	done
 }
+get_yn() {
+	while :; do
+		read -p "$msg" yn
+
+		case $yn in 
+			[yY] )
+				c="Y"
+				break;;
+			[nN] )
+				c="N"
+				break;;
+			[aA] )
+				c="A"
+				break;;
+			* ) echo invalid response;;
+		esac
+	done
+}
 
 clear
 
-if [ ! -d ./assets ]; then
+if [[ ! -d ./assets ]]; then
 	mkdir -p ./assets;
 fi
-if [ ! -d ./assets/audio ]; then
+if [[ ! -d ./assets/audio ]]; then
 	mkdir -p ./assets/audio;
 fi
-if [ ! -d ./assets/models ]; then
+if [[ ! -d ./assets/models ]]; then
 	mkdir -p ./assets/models;
 fi
-if [ ! -d ./assets/textures ]; then
+if [[ ! -d ./assets/textures ]]; then
 	mkdir -p ./assets/textures;
 fi
 
 echo "1. Pack"
 echo "2. Unpack"
 
-$msg = "Enter the value next to function: "
-input()
-$op = $ret
+msg="Enter the value next to function: "
+get_input
+oper=$ret
 
 echo "1. Audio"
 echo "2. Models"
@@ -41,76 +61,46 @@ echo "3. Textures"
 echo "4. All"
 echo "5. Models and Textures"
 
-$msg = "Enter number next to media type: "
-input()
-$media = $ret
+upper=5
+msg="Enter number next to media type: "
+get_input
+media=$ret
 
-if [$op -eq 2]; then
+if [[ $oper -eq 2 ]]; then
 
-	$msg = "Should these files overwrite the current files? (Y Yes, N No, A Ask): " c
-	if ["$c" = "Y"]; then
-		ow = "-o+"
-	elif ["$c" = "N"]; then
-		ow = "-o-"
-	elif ["$c" = "A"]; then
-		ow = "-o"
-	fi
-	
-	if [ $media -eq 5 ]; then
-		echo "Unpacking Models"
-		echo "--------------------------"
-		unrar x $ow ./audio.assets ./assets/audio
-		echo "--------------------------"
+	msg="Should these files overwrite the current files? (Y Yes, N No, A Ask): "
+	get_yn
+	if [[ "$c" = "Y" ]]; then
+		ow="-o+"
+	elif [[ "$c" = "N" ]]; then
+		ow="-o-"
+	elif [[ "$c" = "A" ]]; then
+		ow="-o"
 	fi
   
-	if [ $media -eq 4 ]; then
-		echo "Unpacking Models"
-		echo "--------------------------"
-		unrar x $ow ./models.assets ./assets/models
-		echo "--------------------------"
-	fi
-  
-	if [ $media -eq 3 ]; then
+	if [ $media -eq 3 ] || [ $media -eq 5 ] || [ $media -eq 4 ]; then
 		echo "Unpacking Textures"
 		echo "--------------------------"
 		unrar x $ow ./textures.assets ./assets/textures
 		echo "--------------------------"
 	fi
   
-	if [ $media -eq 2 ]; then
+	if [ $media -eq 2 ] || [ $media -eq 5 ] || [ $media -eq 4 ]; then
 		echo "Unpacking Models"
 		echo "--------------------------"
 		unrar x $ow ./models.assets ./assets/models
 		echo "--------------------------"
 	fi
   
-	if [ $media -eq 1 ]; then
+	if [ $media -eq 1 ] || [ $media -eq 5 ]; then
 		echo "Unpacking Audio"
 		echo "--------------------------"
 		unrar x $ow ./audio.assets ./assets/audio
 		echo "--------------------------"
 	fi
 	
-elif [$op -eq 1]; then
-	if [ $media -eq 5 ]; then
-		echo "Packing Models"
-		echo "--------------------------"
-		cd ./assets/models
-		rar a ../../models.assets .
-		echo "--------------------------"
-		cd ../..
-	fi
-  
-	if [ $media -eq 4 ]; then
-		echo "Packing Audio"
-		echo "--------------------------"
-		cd ./assets/audio
-		rar a ../../audio.assets .
-		echo "--------------------------"
-		cd ../..
-	fi
-  
-	if [ $media -eq 3 ]; then
+elif [[ $oper -eq 1 ]]; then
+	if [ $media -eq 3 ] || [ $media -eq 5 ] || [ $media -eq 4 ]; then
 		echo "Packing Textures"
 		echo "--------------------------"
 		cd ./assets/textures
@@ -119,7 +109,7 @@ elif [$op -eq 1]; then
 		cd ../..
 	fi
   
-	if [ $media -eq 2 ]; then
+	if [ $media -eq 2 ] || [ $media -eq 5 ] || [ $media -eq 4 ]; then
 		echo "Packing Models"
 		echo "--------------------------"
 		cd ./assets/models
@@ -128,7 +118,7 @@ elif [$op -eq 1]; then
 		cd ../..
 	fi
   
-	if [ $media -eq 1 ]; then
+	if [ $media -eq 1 ] || [ $media -eq 5 ]; then
 		echo "Packing Audio"
 		echo "--------------------------"
 		cd ./assets/audio
